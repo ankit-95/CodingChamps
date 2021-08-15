4be49cc0a24b0236e54f0751e5293c12130f7edd
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-08-15 23:10:41 +0530

    Change : Pattern Problem added

---
+++ src/com/codingchamps/GeeksForGeeks/PatternProb1.java
@@ -0,0 +1,27 @@
+package com.codingchamps.GeeksForGeeks;
+
+import java.util.Scanner;
+
+/*
+Pattern to be printed :
+            *
+           **
+          ***
+         ****
+        *****
+*/
+public class PatternProb1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=n;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
