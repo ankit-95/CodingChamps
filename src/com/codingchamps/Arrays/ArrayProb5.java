5553e553a05eaae059447446d11125c882055d58
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-08-27 16:07:32 +0530

    Array: Prob5

--- src/com/codingchamps/Arrays/ArrayProb5.java
+++ src/com/codingchamps/Arrays/ArrayProb5.java
@@ -2,6 +2,22 @@ package com.codingchamps.Arrays;
 
 import java.util.Arrays;
 
+/*
+Double the first element and move zero to end
+        Difficulty Level : Easy
+        Last Updated : 24 May, 2021
+        For a given array of n integers and assume that ‘0’ is an invalid number and all others as a valid number. Convert the array in such a way that if both current and next element is valid then double current value and replace the next number with 0. After the modification, rearrange the array such that all 0’s shifted to the end.
+        Examples:
+
+        Input : arr[] = {2, 2, 0, 4, 0, 8}
+        Output : 4 4 8 0 0 0
+
+        Input : arr[] = {0, 2, 2, 2, 0, 6, 6, 0, 0, 8}
+        Output :  4 2 12 8 0 0 0 0 0 0
+
+        Time Complexity: O(n).
+*/
+
 public class ArrayProb5 {
 
     public static void modifyArray(int ar[]) {
