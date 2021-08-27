59d28bee7c3bce5eccc88a9ad7b323497934eb9a
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-08-27 15:02:49 +0530

    Array Prob 5 : Move Zero Code optimized

--- src/com/codingchamps/Arrays/ArrayProb5.java
+++ src/com/codingchamps/Arrays/ArrayProb5.java
@@ -11,7 +11,7 @@ public class ArrayProb5 {
                 ar[i+1] = 0;
             }
         }
-        int index = 0;
+       /* int index = 0;
         for (int i = 0; i < ar.length; i++) {
             if (ar[i] != 0) {
                 ar[index++]  = ar[i];
@@ -19,7 +19,22 @@ public class ArrayProb5 {
         }
         while(index < ar.length){
             ar[index++] = 0;
+        }*/
+        int lastSeenPositiveIndex = 0;
+        for( int index = 0; index < ar.length; index++)
+        {
+            if(ar[index] != 0)
+            {
+                swap(ar,index, lastSeenPositiveIndex);
+                lastSeenPositiveIndex++;
+            }
         }
+    }
+    public static void swap(int[] A, int i, int j)
+    {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
     public static void main(String args[]){
         int ar[] = {0,0, 0, 0, 0, 9, 0, 31, 31, 0, 0, 12, 46, 0, 4};
