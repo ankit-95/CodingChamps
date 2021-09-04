d02bcbad565d253cd940b96045ca81d7842edead
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-09-04 17:11:13 +0530

    String : Palindrome LeetCode Solution

--- src/com/codingchamps/GeeksForGeeks/PalindromeString.java
+++ src/com/codingchamps/GeeksForGeeks/PalindromeString.java
@@ -2,6 +2,26 @@ package com.codingchamps.GeeksForGeeks;
 
 public class PalindromeString {
 
+
+    public static boolean checkPalindrome(String s){
+        int start=0;
+        int last=s.length()-1;
+        for(; start < s.length()/2;){
+            if(Character.isLetter(s.charAt(start)) && Character.isLetter(s.charAt(last))){
+                if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(last))){
+                    return false;
+                }
+                start++;
+                last--;
+            } if(!Character.isLetter(s.charAt(start))){
+                start++;
+            } if(!Character.isLetter(s.charAt(last))){
+                last--;
+            }
+        }
+        return true;
+    }
+
     public static int isPlaindrome(String s) {
 
         for(int i=0, j = s.length()-1;i<s.length()/2;i++,j--){
@@ -15,5 +35,7 @@ public class PalindromeString {
 
         System.out.println(isPlaindrome("abba"));
         System.out.println(isPlaindrome("abc"));
+
+        System.out.println(checkPalindrome("A man, a plan, a canal: Panama"));
     }
 }
