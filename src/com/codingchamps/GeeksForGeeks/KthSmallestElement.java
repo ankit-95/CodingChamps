effa44787f3d8411547cd2e03bbcad96c3d78432
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-09-21 22:33:20 +0530

    Renamed to KthSmallestElement

--- src/com/codingchamps/GeeksForGeeks/KthSmallestElement.java
+++ src/com/codingchamps/GeeksForGeeks/KthSmallestElement.java
@@ -3,7 +3,32 @@ package com.codingchamps.GeeksForGeeks;
 //Using Heap Data structure
 import java.util.Arrays;
 
-public class KthLargestElement {
+/*Kth smallest element
+        Medium Accuracy: 46.66% Submissions: 100k+ Points: 4
+        Given an array arr[] and an integer K where K is smaller than size of array,
+        the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+        Example 1:
+        Input:
+        N = 6
+        arr[] = 7 10 4 3 20 15
+        K = 3
+        Output : 7
+        Explanation :
+        3rd smallest element in the given
+        array is 7.
+
+        Example 2:
+        Input:
+        N = 5
+        arr[] = 7 10 4 20 15
+        K = 4
+        Output : 15
+        Explanation :
+        4th smallest element in the given
+        array is 15.
+        */
+public class KthSmallestElement {
 
     public static void buildHeap(int ar[], int n){
 
@@ -30,12 +55,12 @@ public class KthLargestElement {
         }
     }
 
-    public static void findkthLargest(int ar[],int k){
+    public static void findkthSmallest(int ar[],int k){
         buildHeap(ar,k);
         for(int i=k;i<ar.length;i++){
             if(ar[i] < ar[0]){
                 ar[0] = ar[i];
-                heapify(ar,ar.length,0);
+                heapify(ar,k,0);
             }
         }
         Arrays.stream(ar).forEach(e-> System.out.print(e+" "));
@@ -44,9 +69,9 @@ public class KthLargestElement {
     public static void main(String args[]){
 
         int ar[] = { 12, 3, 5, 7, 19 };
-        int k = 2;
+        int k = 3;
 
-        findkthLargest(ar,k);
+        findkthSmallest(ar,k);
 
     }
 }
