2e3d5879dfda28eee21bcd8010d658f99f033e7a
ankit-95 <ankitgv.kvtagore@gmail.com>
2021-10-18 18:43:18 +0530

    GFG : Print Duplicates in String

---
+++ src/com/codingchamps/GeeksForGeeks/PrintDuplicatesInString.java
@@ -0,0 +1,48 @@
+package com.codingchamps.GeeksForGeeks;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrintDuplicatesInString {
+
+	public static void approach1(String s){
+	    int count[] = new int[256];
+
+	    for(int i=0;i<s.length();i++){
+	      count[s.charAt(i)]++;
+	    }
+
+	    for(int i=0;i<255;i++){
+	      if(count[i] > 1)
+	        System.out.println((char)i +", count = "+ count[i]);
+	    }
+	  }
+
+	  public static void approach2(String s){
+	    HashMap<Character, Integer> count = new HashMap<>();
+
+	    for(int i=0;i<s.length();i++){
+	      if(count.containsKey(s.charAt(i))){
+	        count.put(s.charAt(i), count.get(s.charAt(i))+1);
+	      } else {
+	        count.put(s.charAt(i), 1);  
+	      }
+	    }
+
+	    for(Map.Entry e : count.entrySet()){
+	      char key = (char)e.getKey();
+	      int value = (Integer)e.getValue();
+
+	      if(value > 1)
+	        System.out.println(key +", count = "+ value);
+	    }
+	  }
+	  
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "test string";
+		
+		approach2(s);
+	}
+
+}
