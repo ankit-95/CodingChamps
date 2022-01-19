c7106e924c08bc7a877fb9ed85ca1961b1508219
ankit-95 <ankitgv.kvtagore@gmail.com>
2022-01-19 13:24:41 +0530

    LL : Link added for 2b7bca8 commit

--- src/com/codingchamps/LinkedList/RemoveDuplicatesInSortedLL.java
+++ src/com/codingchamps/LinkedList/RemoveDuplicatesInSortedLL.java
@@ -1,5 +1,11 @@
 package com.codingchamps.LinkedList;
 
+//Remove duplicate element from sorted Linked List
+//        Given a singly linked list consisting of N nodes. The task is to remove duplicates (nodes with duplicate values) from the
+//        given list (if exists).
+//        Note: Try not to use extra space. Expected time complexity is O(N). The nodes are arranged in a sorted way.
+// Link : https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1
+
 public class RemoveDuplicatesInSortedLL {
     Node head;
     static int count;
