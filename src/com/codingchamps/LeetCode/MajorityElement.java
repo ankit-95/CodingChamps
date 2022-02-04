bd24f39fbb9c4d9c4d78ad1011b5a2cc0597e3fe
ankit-95 <ankitgv.kvtagore@gmail.com>
2022-02-04 19:35:35 +0530

    LeetCode : Some changes in Language

---
+++ src/com/codingchamps/LeetCode/MajorityElement.java
@@ -0,0 +1,21 @@
+package com.codingchamps.LeetCode;
+
+public class MajorityElement {
+
+    // Time Complexity : O(n) and Aux Space : O(1)
+    // Concpet is base don Booyre Moore Text Matching Algorithm
+    public static int getMajorityElement(int ar[]){
+        int count=0;
+        Integer candidate = null;
+        for(int num : ar){
+            if(count==0)
+                candidate = num;
+            count += (num==candidate)?1:-1;
+        }
+        return candidate;
+    }
+    public static void main(String[] args) {
+        int ar[] = {2,2,1,1,1,2,2};
+        System.out.println(getMajorityElement(ar));
+    }
+}
