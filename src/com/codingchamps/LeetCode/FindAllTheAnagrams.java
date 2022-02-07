31ad1f1eadb53165ecf3706c0d0421909fdb624f
ankit-95 <ankitgv.kvtagore@gmail.com>
2022-02-07 22:31:40 +0530

    LeetCode : Find all the anagrams in String

---
+++ src/com/codingchamps/LeetCode/FindAllTheAnagrams.java
@@ -0,0 +1,49 @@
+package com.codingchamps.LeetCode;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Link : https://leetcode.com/problems/find-all-anagrams-in-a-string/submissions/
+public class FindAllTheAnagrams {
+
+    public static List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+
+        int[] hash = new int[256];
+
+        for (char c : p.toCharArray()) {
+            hash[c]++;
+        }
+        int left = 0, right = 0, count = p.length();
+
+        while (right < s.length()) {
+            if (hash[s.charAt(right)] >= 1) {
+                count--;
+            }
+            hash[s.charAt(right)]--;
+            right++;
+
+            if (count == 0) {
+                list.add(left);
+            }
+            if (right - left == p.length()) {
+
+                if (hash[s.charAt(left)] >= 0) {
+                    count++;
+                }
+                hash[s.charAt(left)]++;
+                left++;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findAnagrams("af","be"));
+        System.out.println(findAnagrams("cbaebabacd","abc"));
+
+    }
+}
