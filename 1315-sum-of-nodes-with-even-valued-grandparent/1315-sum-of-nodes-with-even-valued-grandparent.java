358ac5d20f0cd360d913668a54b48dd55f8b6a44
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-04-09 23:09:40 +0530

    Time: 8 ms (14.50%), Space: 54.3 MB (5.56%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -15,39 +15,32 @@
  */
 class Solution {
     public int sumEvenGrandparent(TreeNode root) {
-        Map<TreeNode,TreeNode> parent = new HashMap<>();
         Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        parent.put(root,null);
-        while(!q.isEmpty()){
-            TreeNode temp = q.remove();
-            if(temp.left!=null){
-                parent.put(temp.left,temp);
-                q.add(temp.left);
-            }
-            if(temp.right!=null){
-                parent.put(temp.right,temp);
-                q.add(temp.right);
-            }
-        }
-        Map<TreeNode,TreeNode> gParent = new HashMap<>();
-        for(Map.Entry<TreeNode,TreeNode> pair : parent.entrySet()){
-            gParent.put(pair.getKey(),parent.get(pair.getValue()));
-        }
-        
-        q = new LinkedList<>();
         q.add(root);
         int sum=0;
         while(!q.isEmpty()){
             TreeNode temp = q.remove();
-            if(gParent.containsKey(temp) && gParent.get(temp)!=null && gParent.get(temp).val%2==0){
-                sum+=temp.val;
-            }
             if(temp.left!=null){
                 q.add(temp.left);
+                if(temp.val%2==0){
+                    if(temp.left.left!=null){
+                        sum+=temp.left.left.val;
+                    }
+                    if(temp.left.right!=null){
+                        sum+=temp.left.right.val;
+                    }
+                }   
             }
             if(temp.right!=null){
                 q.add(temp.right);
+                if(temp.val%2==0){
+                    if(temp.right.left!=null){
+                        sum+=temp.right.left.val;
+                    }
+                    if(temp.right.right!=null){
+                        sum+=temp.right.right.val;
+                    }   
+                }
             }
         }
         
