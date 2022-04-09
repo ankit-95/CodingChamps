a0517b1486dcd26cae28fb6b6bb1ce08fcf73b9a
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-04-09 17:35:06 +0530

    Time: 16 ms (44.95%), Space: 119.2 MB (65.83%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,13 +1,31 @@
 class Solution {
     
+    public int findParent(int i,int parent[]){
+        if(parent[i]==i)
+            return i;
+        return parent[i] = findParent(parent[i],parent);
+    }
+    
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
         List<Integer> ans = new ArrayList<>();    
-        int nodes[] = new int[n];
-        for(List<Integer> edge : edges){
-            nodes[edge.get(1)]=1;
-        }
+        int parent[] = new int[n];
         for(int i=0;i<n;i++){
-            if(nodes[i]==0)
+            parent[i] = i;
+        }
+        boolean visited[] = new boolean[n];
+        for(int i=0;i<edges.size();i++){
+            List<Integer> curr = edges.get(i);
+            int sv = curr.get(0);
+            int ev = curr.get(1);
+            if(visited[ev]) continue;
+            sv = findParent(sv,parent);
+            ev = findParent(ev,parent);
+            parent[ev] = sv;
+            visited[ev] = true;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(parent[i]==i)
                 ans.add(i);
         }
         return ans;
