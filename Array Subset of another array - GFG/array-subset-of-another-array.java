eed6f98d9f5d3675065addf5fd7a1b15f2b084fd
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-04-15 00:51:04 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.java
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        
+        Map<Long,Integer> map = new HashMap<>();
+        for(int i=0;i<a2.length;i++){
+            map.put(a2[i],map.getOrDefault(a2[i],0)+1);
+        }
+        
+        for(int i=0;i<a1.length;i++){
+            if(map.containsKey(a1[i])){
+                map.put(a1[i],map.getOrDefault(a1[i],0)-1);
+            }
+        }
+        
+        for(Map.Entry<Long,Integer> pair : map.entrySet()){
+            if(pair.getValue()!=0)
+                return "No";
+        }
+        return "Yes";
+    }
+}
