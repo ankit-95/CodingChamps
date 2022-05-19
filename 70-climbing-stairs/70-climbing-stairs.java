88434c3d8e6931fcbd41907e87f55bef42baa899
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-19 21:56:27 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (41.09%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    // public void helper(int n, String s, List<String> ans){
+    //     if(n==0){
+    //         ans.add(s);
+    //         return;
+    //     }
+    //     for(int i=1;i<=2;i++){
+    //         if(n-i>=0)
+    //             helper(n-i,s+i,ans);    
+    //     } 
+    // }
+    // public int climbStairs(int n) {
+    //     List<String> ans = new ArrayList<>();
+    //     helper(n,"",ans);
+    //     //System.out.print(ans);
+    //     return ans.size();
+    // }
+    
+    public int climbStairs(int n) {
+        if(n<=1)
+            return 1;
+        int prev1 = 1;
+        int prev2 = 2;
+        for(int i=3;i<=n;i++){
+            int newVal = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = newVal;
+        }
+        return prev2;
+    }
+    
+    
+}
