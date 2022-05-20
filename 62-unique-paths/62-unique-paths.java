1f457d7003e2425063d87ebcf63c0f0270ec0e1c
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-20 18:55:46 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (58.57%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,30 @@
+class Solution {
+    // Recursion solution
+    
+    // int count = 0;
+    // public void helper(int i,int j,int m,int n){
+    //     if(i< 0 || i>=m || j<0 || j>=n)
+    //         return;
+    //     if(i==m-1 && j==n-1){
+    //         count++;
+    //     }
+    //     helper(i+1,j,m,n);
+    //     helper(i,j+1,m,n);
+    // }
+    // public int uniquePaths(int m, int n) {
+    //     helper(0,0,m,n);
+    //     return count;
+    // }
+    
+    // DP
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m+1][n+1];
+        dp[0][1] = 1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }    
+        }
+        return dp[m][n];
+    }
+}
