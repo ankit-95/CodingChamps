f5a055ae4072f07ac54f170f3906a16c698f3e73
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-21 20:13:59 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (45.38%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -15,32 +15,32 @@
  */
 class Solution {
     
-    // Recursive Solution
-    // public TreeNode invertTree(TreeNode root) {
-    //     if(root==null)
-    //         return null;
-    //     final TreeNode left = root.left, right = root.right;
-    //     root.left = invertTree(right);
-    //     root.right = invertTree(left);
-    //     return root;    
-    // }
-    
-    // BFS Solution
+    //Recursive Solution
     public TreeNode invertTree(TreeNode root) {
         if(root==null)
-            return root;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode temp = q.remove();
-            TreeNode left = temp.left;
-            temp.left = temp.right;
-            temp.right = left;
-            if(temp.left!=null)
-                q.add(temp.left);
-            if(temp.right!=null)
-                q.add(temp.right);
-        }
-        return root;
+            return null;
+        final TreeNode left = root.left, right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;    
     }
+    
+    // BFS Solution
+    // public TreeNode invertTree(TreeNode root) {
+    //     if(root==null)
+    //         return root;
+    //     Queue<TreeNode> q = new LinkedList<>();
+    //     q.add(root);
+    //     while(!q.isEmpty()){
+    //         TreeNode temp = q.remove();
+    //         TreeNode left = temp.left;
+    //         temp.left = temp.right;
+    //         temp.right = left;
+    //         if(temp.left!=null)
+    //             q.add(temp.left);
+    //         if(temp.right!=null)
+    //             q.add(temp.right);
+    //     }
+    //     return root;
+    // }
 }
