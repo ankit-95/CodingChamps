140c5cfebbe8da1b71a03514f2792f0c6c767630
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-26 20:29:07 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            for (int i = 0; i < V + 1; i++)
+                list.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                list.get(u).add(v);
+            }
+            if (new Solution().isCyclic(V, list) == true)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to detect cycle in a directed graph.
+    //using kahn's Algo, try to generate toposort
+    
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        int indegree[] = new int[V];
+        for(int i=0;i<V;i++){
+            for(int v : adj.get(i))
+                indegree[v]++;
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+                q.add(i);
+        }
+        
+        int count=0;
+        while(!q.isEmpty()){
+            int u = q.remove();
+            count++;
+            for(int v : adj.get(u)){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.add(v);
+                }
+            }
+        }
+        
+        if(count==V) return false;
+        return true;
+    }
+}
