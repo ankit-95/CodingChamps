1cb94dded8a6331ae75686b0001a9329a4bebd18
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-29 23:34:37 +0530

    Time: 2 ms (76.17%), Space: 44.2 MB (69.81%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.java
@@ -14,27 +14,47 @@
  * }
  */
 class Solution {
-    public List<Integer> largestValues(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
+    // BFS 
+//     public List<Integer> largestValues(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(root==null)
+//             return ans;
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+        
+        
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             int max = Integer.MIN_VALUE;
+//             while(size-->0){
+//                 TreeNode temp = q.remove();
+//                 max = Math.max(max, temp.val);
+//                 if(temp.left!=null)
+//                     q.add(temp.left);
+//                 if(temp.right!=null)
+//                     q.add(temp.right);
+//             }
+//             ans.add(max);
+//         }
+//         return ans;
+//     }
+    
+    //DFS
+     public List<Integer> largestValues(TreeNode root) {
+         List<Integer> ans = new ArrayList<>();
+         helper(root,ans,0);
+         return ans;
+    }
+    
+    public void helper(TreeNode root,List<Integer> ans,int level){
         if(root==null)
-            return ans;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            int max = Integer.MIN_VALUE;
-            while(size-->0){
-                TreeNode temp = q.remove();
-                max = Math.max(max, temp.val);
-                if(temp.left!=null)
-                    q.add(temp.left);
-                if(temp.right!=null)
-                    q.add(temp.right);
-            }
-            ans.add(max);
+            return;
+        if(level == ans.size()){
+            ans.add(root.val);
+        } else {
+            ans.set(level, Math.max(ans.get(level),root.val));
         }
-        return ans;
+        helper(root.left,ans,level+1);
+        helper(root.right,ans,level+1);
     }
 }
