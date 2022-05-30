2759cc945213ed05d072f3ddcc381783ee13e248
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-05-30 17:59:52 +0530

    Time: 240 ms (5.10%), Space: 73.1 MB (32.94%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -0,0 +1,33 @@
+class Solution {
+    
+    public int find(int parent[],int u){
+        if(u!=parent[u]){
+            parent[u] = find(parent,parent[u]);
+        }
+        return parent[u];
+    }
+    public int makeConnected(int n, int[][] connections) {
+        
+        int parent[] = new int[n];
+        for(int i=0;i<n;i++)
+            parent[i] = i;
+        int extraEdges = 0;
+        for(int c[] : connections){
+            int u = c[0];
+            int v = c[1];
+            if(find(parent,u)!=find(parent,v)){
+                parent[find(parent,v)] = find(parent,u);
+            } else {
+                extraEdges++;
+            }
+        }
+        int components = 0;
+        for(int i= 0;i<n;i++){
+            System.out.print(parent[i]+" ");
+            if(i==parent[i])
+              components++;  
+        }
+        
+        return extraEdges>=(components-1) ? (components-1) : -1;
+    }
+}
