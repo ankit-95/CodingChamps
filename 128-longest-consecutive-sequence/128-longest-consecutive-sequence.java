3656f98934ea9806069f121900a4ac94f9deb0cf
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-09 11:15:19 +0530

    Time: 71 ms (47.59%), Space: 82.1 MB (30.68%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,26 +1,55 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0;i<nums.length;i++)
-            set.add(nums[i]);
-        
+    
+    int parent[];
+    int size[];
+    
+    public void union(int a,int b){
+        a = find(a,parent);
+        b = find(b,parent);
+        parent[a] = b;
+        size[b] += size[a];
+    }
+    
+    public int find(int a,int parent[]){
+        if(parent[a]!=a){
+            parent[a] = find(parent[a],parent);
+        }
+        return parent[a];
+    }
+    
+    public int getMaxComponent(){
         int max = 0;
-        for(int i=0;i<nums.length;i++){
-            int count= 1;
-            int num = nums[i];
-            while(set.contains(--num)){
-                count++;
-                set.remove(num);
+        for(int i=0;i<parent.length;i++){
+            if(i==parent[i] && size[i]>max){
+                max = size[i];
             }
-            
-            num = nums[i];
-            while(set.contains(++num)){
-                count++;
-                set.remove(num);
-            }
-            
-            max = Math.max(max,count);
         }
         return max;
+    }
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        parent = new int[n];
+        size = new int[n];
+        
+        for(int i=0;i<n;i++){
+           parent[i] = i;
+           size[i] = 1;
+        }
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])) continue;
+        
+            if(map.containsKey(nums[i]-1)){
+                union(i,map.get(nums[i]-1));
+            }
+            if(map.containsKey(nums[i]+1)){
+                    union(i,map.get(nums[i]+1));
+            }
+            map.put(nums[i],i);
+        }
+        
+        return getMaxComponent();
     }
 }
