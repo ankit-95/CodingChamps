affe23dcf58c48ec03317d38b8a90b7f3391db2c
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-15 20:23:56 +0530

    Time: 3 ms (87.99%), Space: 42.3 MB (50.08%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1) return "1"; 
+        String prev = countAndSay(n - 1); 
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+        while(i < prev.length()) {
+            char curr = prev.charAt(i);
+            int j = 0;
+            while(i+j < prev.length() && prev.charAt(i+j) == curr) j++;
+            str.append(j);
+            str.append(curr);
+            i += j;
+        }
+        return str.toString();
+    }
+}
