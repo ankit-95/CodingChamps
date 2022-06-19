80c310a8d949cd4658ee4a2a7bf05f3368b44f57
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-19 12:51:09 +0530

    Time: 67 ms (47.04%), Space: 74.6 MB (37.07%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,34 @@
+class Solution {
+   class Trie {
+        Trie[] sub = new Trie[26];
+        LinkedList<String> suggestion = new LinkedList<>();
+    }
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Trie root = new Trie();
+        for (String p : products) { // build Trie.
+            insert(p, root); // insert a product into Trie.
+        }
+        return search(searchWord, root);
+    }
+    private void insert(String p, Trie root) {
+        Trie t = root;
+        for (char c : p.toCharArray()) { // insert current product into Trie.
+            if (t.sub[c - 'a'] == null)
+                t.sub[c - 'a'] = new Trie();
+            t = t.sub[c - 'a'];
+            t.suggestion.offer(p); // put products with same prefix into suggestion list.
+            Collections.sort(t.suggestion);
+            if (t.suggestion.size() > 3) // maintain 3 lexicographically minimum strings.
+                t.suggestion.pollLast();        
+        }
+    }
+    private List<List<String>> search(String searchWord, Trie root) {
+        List<List<String>> ans = new ArrayList<>();
+        for (char c : searchWord.toCharArray()) { // search product.
+            if (root != null) // if there exist products with current prefix.
+                root = root.sub[c - 'a'];
+            ans.add(root == null ? Arrays.asList() : root.suggestion); // add it if there exist products with current prefix.
+        }
+        return ans;
+    }
+}
