11b736eb6d417ed57071c783272fe4afe1a27a37
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-21 00:49:20 +0530

    Time: 93 ms (14.03%), Space: 53.8 MB (6.05%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
         int minDis = Integer.MAX_VALUE;
         int res =0;
-        for(int i=0;i<nums.length-2;i++){
+        for(int i=0;i<nums.length;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
                 
                 int l = i+1, r = nums.length-1;
