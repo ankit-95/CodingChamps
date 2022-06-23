72323acd8d7d921285bfbede1c866caba070486c
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-23 19:36:22 +0530

    Time: 2 ms (75.85%), Space: 44.6 MB (13.75%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.java
+++ 174-dungeon-game/174-dungeon-game.java
@@ -19,6 +19,16 @@ class Solution {
         int m = dungeon.length;
         int n = dungeon[0].length;
         int dp[][] = new int[m+1][n+1];
-        return helper(0,0,dungeon,dp);
+        for(int i=0;i<=m;i++)
+            Arrays.fill(dp[i],(int)1e9);
+        dp[m][n-1] = 1;
+        dp[m-1][n] = 1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                int minReq = Math.min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j];
+                dp[i][j] = minReq<=0 ? 1 : minReq;
+            }
+        }
+        return dp[0][0];
     }
 }
