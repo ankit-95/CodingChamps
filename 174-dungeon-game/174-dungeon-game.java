2ccef546a2f9a709319aefc4f878961dcf77b8b9
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-06-23 18:48:51 +0530

    Time: 1 ms (98.12%), Space: 43.1 MB (80.04%) - LeetHub

---
+++ 174-dungeon-game/174-dungeon-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public int helper(int i,int j,int[][] dungeon,int dp[][]){
+        if(i>=dungeon.length || j>=dungeon[0].length){
+            return (int)1e9;
+        }
+        if(dp[i][j]>0)
+            return dp[i][j];
+        if(i==dungeon.length-1 && j==dungeon[0].length-1)
+            return dp[i][j] = dungeon[i][j]<=0 ? -dungeon[i][j]+1 : 1;
+        
+        int right = helper(i,j+1,dungeon,dp);
+        int down = helper(i+1,j,dungeon,dp);
+        int minReq = Math.min(right,down) - dungeon[i][j];
+        return dp[i][j] = minReq<=0 ? 1 : minReq;
+    }
+    
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int dp[][] = new int[m+1][n+1];
+        return helper(0,0,dungeon,dp);
+    }
+}
