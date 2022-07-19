70ca3afc976b6595903eba02651600b04f79dd4c
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-07-19 12:37:30 +0530

    Time: 1 ms (72.34%), Space: 41.6 MB (71.55%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,39 +1,18 @@
 class Solution {
-    // Matrix based solution but more space is consumed.
-    // public List<List<Integer>> generate(int numRows) {
-    //     int mat[][] = new int[numRows][numRows];
-    //     for(int i=0;i<numRows;i++){
-    //         mat[i][0] = 1;
-    //         mat[0][i] = 1;
-    //     }
-    //     for(int i=1;i<numRows;i++){
-    //         for(int j=1;j<numRows;j++){
-    //             mat[i][j] = mat[i-1][j] + mat[i][j-1];
-    //         }    
-    //     }
-    //     List<List<Integer>> ans = new ArrayList<>();
-    //     for(int i=0;i<numRows;i++){
-    //         List<Integer> list = new ArrayList<>();
-    //         for(int j=0;j<=i;j++){
-    //             list.add(mat[i-j][j]);
-    //         }    
-    //         ans.add(new ArrayList<>(list));
-    //     }
-    //     return ans;
-    // }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> res,prev = new ArrayList<>();
+        List<Integer> cur,prev = null;
         for(int i=0;i<numRows;i++){
-            res = new ArrayList<>();
+            cur = new ArrayList<>();
             for(int j=0;j<=i;j++){
-                if(j==0 || j==i)
-                    res.add(1);
-                else 
-                    res.add(prev.get(j) + prev.get(j-1));
-            }    
-            prev = res;
-            ans.add(res);
+                if(j==0 || j==i){
+                    cur.add(1);
+                } else {
+                    cur.add(prev.get(j)+prev.get(j-1));
+                }
+            }
+            prev = cur;
+            ans.add(new ArrayList<>(cur));
         }
         return ans;
     }
