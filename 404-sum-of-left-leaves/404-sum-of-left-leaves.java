25ef1aff005ae488b79af247d8fea2b129949d46
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-07-19 23:12:06 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (39.85%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -18,14 +18,14 @@ class Solution {
     public int helper(TreeNode root){
         if(root==null)
             return 0;
+        if(root.left==null && root.right==null)
+            return root.val;
         int l = helper(root.left);    
         int r = helper(root.right);    
         if(l!=0){
             leftSum+=l;
         }
-        if(root.left==null && root.right==null)
-            return root.val;
-        else return 0;
+        return 0;
     }
     public int sumOfLeftLeaves(TreeNode root) {
         helper(root);
