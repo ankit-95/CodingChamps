f5f62d531dce5241f4281841dda44e459da1ca7b
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-07-22 13:30:57 +0530

    Time: 172 ms (5.20%), Space: 72.3 MB (5.20%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int helper(int index,String s,int dp[]){
+        if(index==s.length()){
+            return 1;   
+        }
+        if(s.charAt(index)=='0')
+            return 0;
+        if(index==s.length()-1)
+            return 1;
+        if(dp[index]>0) return dp[index];
+        int ans =helper(index+1,s,dp);
+        if(Integer.parseInt(s.substring(index,index+2))<=26) {
+            ans += helper(index+2,s,dp);
+        }
+        return dp[index] = ans;
+    }
+    public int numDecodings(String s) {
+        int dp[] = new int[s.length()+1];
+        return helper(0,s,dp);
+    }
+}
