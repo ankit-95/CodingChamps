23e5aea4f8d09dfc78293c977731f6722d0e1dbc
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-08-12 17:54:56 +0530

    Time: 54 ms, Memory: 48.4 MB - LeetHub

---
+++ 3sum-closest/3sum-closest.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums==null || nums.length==0)
+            return 0;
+        Arrays.sort(nums);
+        int minDis = Integer.MAX_VALUE;
+        int res =0;
+        for(int i=0;i<nums.length;i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                
+                int l = i+1, r = nums.length-1;
+                while(l<r){
+                    int sum = nums[i]+nums[l]+nums[r];
+                    if(target==sum){
+                        return target;
+                    } else if(sum> target){
+                          if(sum - target < minDis){
+                              minDis = sum - target;
+                              res = sum;
+                          }
+                          r--;
+                    } else {
+                          if(target - sum < minDis){
+                              minDis = target - sum;
+                              res = sum;
+                          }
+                          l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
