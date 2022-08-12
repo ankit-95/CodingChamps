6c658a048cf4c49567a69a9cecefe24c90444c7f
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-08-12 14:40:50 +0530

    Time: 4 ms, Memory: 43.6 MB - LeetHub

---
+++ two-sum/two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int result[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                result[0] = i;
+                result[1] = map.get(diff);
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+    }
+}
