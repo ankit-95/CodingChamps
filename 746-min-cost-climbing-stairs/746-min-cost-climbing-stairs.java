7df4961eeb63b15fb693a15e9fc5c4f967bcfc22
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-08-19 21:27:01 +0530

    Time: 2 ms (30.88%), Space: 44.1 MB (22.47%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,12 +1,20 @@
 class Solution {
+    
+    public int helper(int []cost, int index,int dp[]){
+        if(index >=cost.length)
+            return 0;
+        if(dp[index]!=-1) return dp[index];
+        int first = Integer.MAX_VALUE,second = Integer.MAX_VALUE;
+        first = cost[index] + helper(cost,index+1,dp);
+        second = cost[index] + helper(cost,index+2,dp);
+        return dp[index] = Math.min(first,second);
+    }
     public int minCostClimbingStairs(int[] cost) {
-        int dp[] = new int[cost.length + 1];
-        dp[0] = 0;
-        dp[1] = cost[0];
-        for(int i=2;i<=cost.length;i++){
-            dp[i]  = cost[i-1] + Math.min(dp[i-1],dp[i-2]);
-        }
-        
-        return Math.min(dp[cost.length],dp[cost.length - 1]);
+        int dp[] = new int[cost.length+1];
+        Arrays.fill(dp,-1);
+        int min_a = helper(cost,0,dp);
+        Arrays.fill(dp,-1);
+        int min_b = helper(cost,1,dp);
+        return Math.min(min_a,min_b);
     }
 }
