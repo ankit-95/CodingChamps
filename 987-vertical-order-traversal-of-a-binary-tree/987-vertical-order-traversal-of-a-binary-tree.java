f299831e8cf4d6f512c77fa2c409b6187b5c841d
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-09-04 12:30:24 +0530

    Time: 4 ms (83.66%), Space: 42.8 MB (76.95%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,68 @@
+class Triplet<F, S, T> {
+    public final F first;
+    public final S second;
+    public final T third;
+
+    public Triplet(F first, S second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
+
+class Solution {
+    List<Triplet<Integer, Integer, Integer>> nodeList = new ArrayList<>();
+
+    private void DFS(TreeNode node, Integer row, Integer column) {
+        if (node == null)
+            return;
+        nodeList.add(new Triplet(column, row, node.val));
+        // preorder DFS traversal
+        this.DFS(node.left, row + 1, column - 1);
+        this.DFS(node.right, row + 1, column + 1);
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> output = new ArrayList();
+        if (root == null) {
+            return output;
+        }
+
+        // step 1). DFS traversal
+        DFS(root, 0, 0);
+
+        // step 2). sort the list by <column, row, value>
+        Collections.sort(this.nodeList, new Comparator<Triplet<Integer, Integer, Integer>>() {
+            @Override
+            public int compare(Triplet<Integer, Integer, Integer> t1,
+                    Triplet<Integer, Integer, Integer> t2) {
+                if (t1.first.equals(t2.first))
+                    if (t1.second.equals(t2.second))
+                        return t1.third - t2.third;
+                    else
+                        return t1.second - t2.second;
+                else
+                    return t1.first - t2.first;
+            }
+        });
+
+        // step 3). extract the values, grouped by the column index.
+        List<Integer> currColumn = new ArrayList();
+        Integer currColumnIndex = this.nodeList.get(0).first;
+
+        for (Triplet<Integer, Integer, Integer> triplet : this.nodeList) {
+            Integer column = triplet.first, value = triplet.third;
+            if (column == currColumnIndex) {
+                currColumn.add(value);
+            } else {
+                output.add(currColumn);
+                currColumnIndex = column;
+                currColumn = new ArrayList();
+                currColumn.add(value);
+            }
+        }
+        output.add(currColumn);
+
+        return output;
+    }
+}
