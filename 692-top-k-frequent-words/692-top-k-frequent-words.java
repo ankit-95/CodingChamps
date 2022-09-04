dc5e6bbe060db9b26b53f8916e48b81f6dbb171f
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-09-04 21:31:29 +0530

    Time: 6 ms (96.18%), Space: 42.6 MB (93.67%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -1,25 +1,22 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        Map<String,Integer> map = new HashMap<>();
-        for(String s : words){
-            map.put(s,map.getOrDefault(s,0)+1);
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
         }
-      //System.out.print(map);
-        PriorityQueue<String> q = new PriorityQueue<>((a,b)->{
-            if(map.get(a)==map.get(b)){
-                return a.compareTo(b);
-            }
-            return map.get(b) - map.get(a);
-        });
-        
-        for(String key : map.keySet()){
-                q.add(key);
+        PriorityQueue<String> h = new PriorityQueue<>(
+                (w1, w2) -> cnt.get(w1).equals(cnt.get(w2)) ? w2.compareTo(w1) : cnt.get(w1) - cnt.get(w2));
+
+        for (String word : cnt.keySet()) {
+            h.offer(word);
+            if (h.size() > k)
+                h.poll();
         }
-        
-        List<String> ans = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            ans.add(q.remove());
-        }
-        return ans;
+
+        List<String> res = new ArrayList<>();
+        while (!h.isEmpty())
+            res.add(h.poll());
+        Collections.reverse(res);
+        return res;
     }
 }
