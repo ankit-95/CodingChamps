ba16da1d05776a34dba21e79cbc645ff512b3a1c
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-09-25 12:53:58 +0530

    Time: 3 ms (100.00%), Space: 42.9 MB (91.44%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -0,0 +1,83 @@
+class Node {
+  public int value;
+  public Node nextNode;
+
+  public Node(int value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class MyCircularQueue {
+
+  private Node head, tail;
+  private int count;
+  private int capacity;
+
+  /** Initialize your data structure here. Set the size of the queue to be k. */
+  public MyCircularQueue(int k) {
+    this.capacity = k;
+  }
+
+  /** Insert an element into the circular queue. Return true if the operation is successful. */
+  public boolean enQueue(int value) {
+    if (this.count == this.capacity)
+      return false;
+
+    Node newNode = new Node(value);
+    if (this.count == 0) {
+      head = tail = newNode;
+    } else {
+      tail.nextNode = newNode;
+      tail = newNode;
+    }
+    this.count += 1;
+    return true;
+  }
+
+  /** Delete an element from the circular queue. Return true if the operation is successful. */
+  public boolean deQueue() {
+    if (this.count == 0)
+      return false;
+    this.head = this.head.nextNode;
+    this.count -= 1;
+    return true;
+  }
+
+  /** Get the front item from the queue. */
+  public int Front() {
+    if (this.count == 0)
+      return -1;
+    else
+      return this.head.value;
+  }
+
+  /** Get the last item from the queue. */
+  public int Rear() {
+    if (this.count == 0)
+      return -1;
+    else
+      return this.tail.value;
+  }
+
+  /** Checks whether the circular queue is empty or not. */
+  public boolean isEmpty() {
+    return (this.count == 0);
+  }
+
+  /** Checks whether the circular queue is full or not. */
+  public boolean isFull() {
+    return (this.count == this.capacity);
+  }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
