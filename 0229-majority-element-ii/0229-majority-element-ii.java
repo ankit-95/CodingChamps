361dbe6d378852ce66460211e9c0db7c38440f4f
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-10-27 23:58:39 +0530

    Time: 8 ms (59.72%), Space: 49.6 MB (66.52%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,7 +3,7 @@ class Solution {
         if (nums == null || nums.length == 0)
 		return new ArrayList<Integer>();
         List<Integer> result = new ArrayList<Integer>();
-        int number1 = nums[0], number2 = nums[0], count1 = 0, count2 = 0, len = nums.length;
+        int number1 = 0, number2 = 0, count1 = 0, count2 = 0, len = nums.length;
         for (int i = 0; i < len; i++) {
             if (nums[i] == number1)
                 count1++;
