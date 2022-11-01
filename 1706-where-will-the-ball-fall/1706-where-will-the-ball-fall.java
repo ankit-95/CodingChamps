9456839d7479ce2157ce00f242fa84d5e9c510c5
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-11-01 12:34:45 +0530

    Time: 4 ms (50.06%), Space: 54.9 MB (8.54%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,16 +1,17 @@
 class Solution {
     
-    public int helper(int i,int j,int grid[][]){
+    public int helper(int i,int j,int grid[][],int dp[][]){
         
         if(i==grid.length){
             return j;
         }
         
+        if(dp[i][j]!=-1) return dp[i][j];
         int next = j + grid[i][j];
         
         if(next<0 || next> grid[0].length-1 || grid[i][j]!=grid[i][next])
             return -1;
-        return helper(i+1,next,grid);
+        return dp[i][j] = helper(i+1,next,grid,dp);
         
     }
     public int[] findBall(int[][] grid) {
@@ -18,8 +19,12 @@ class Solution {
         int m = grid[0].length;
         int ans[] = new int[m];
         
+        int dp[][] = new int[n+1][m];
+        for(int d[] : dp)
+            Arrays.fill(d,-1);
+        
         for(int i=0;i<m;i++){
-            ans[i] = helper(0,i,grid);    
+            ans[i] = helper(0,i,grid,dp);    
         }
         return ans;
     }
