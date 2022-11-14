ba69d8f28b9b7c1c4d51c8a69051540fe8fbe40b
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2022-11-14 09:53:07 +0530

    Time: 450 ms (6.55%), Space: 50 MB (57.39%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void dfs(int[] parent,int[][] stones, HashSet<int[]> vis){
+        vis.add(parent);
+        for(int stone[] : stones){
+            if(!vis.contains(stone)){
+                if(stone[0]==parent[0] || stone[1]==parent[1]){
+                    dfs(stone,stones,vis);
+                }
+            }
+        }
+    }
+    
+    public int removeStones(int[][] stones) {
+        HashSet<int[]> vis = new HashSet<>();
+        int count=0;
+        for(int stone[] : stones){
+            if(!vis.contains(stone)){
+                    dfs(stone,stones,vis);
+                    count++;
+                }
+        }
+        System.out.println(count);
+        return stones.length - count;
+    }
+}
