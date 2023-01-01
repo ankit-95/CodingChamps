996c151fc977cc1bb882c78783006da526dbe836
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2023-01-01 08:23:36 +0530

    Time: 117 ms (64.09%), Space: 140.2 MB (66.85%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    
+    public boolean dfs(int src,boolean visited[],Map<Integer,List<Integer>> map,int destination){
+        visited[src] = true;
+        if(src==destination)
+            return true;
+        for(int v : map.getOrDefault(src,new ArrayList<>())){
+            if(!visited[v]){
+                if(dfs(v,visited,map,destination))
+                    return true;
+            } 
+        }
+        return false;
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        Map<Integer,List<Integer>> adj = new HashMap<>();
+        for(int i=0;i<edges.length;i++){
+            adj.putIfAbsent(edges[i][0],new ArrayList<>());
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.putIfAbsent(edges[i][1],new ArrayList<>());
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        boolean visited[] = new boolean[n];
+        //return dfs(source,visited,adj,destination);
+        //BFS Approach
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        visited[source] = true;
+        while(!q.isEmpty()){
+            int u = q.remove();
+            if(u==destination)
+                return true;
+            for(int v : adj.getOrDefault(u, new ArrayList<>())){
+                if(!visited[v]){
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+        }
+        return false;
+    }
+}
