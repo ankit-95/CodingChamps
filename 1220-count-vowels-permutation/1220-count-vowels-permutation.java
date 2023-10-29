b6c85c8d8ad5e6a7598e78504b77c3a00bd4f405
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2023-10-29 09:40:11 +0530

    Time: 17 ms (51.48%), Space: 43.2 MB (64.92%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -7,7 +7,7 @@ class Solution {
         }
         
         for(int i=1;i<n;i++){
-            dp[i+1][0] = ((dp[i][1] + dp[i][2])% mod +dp[i][4]) % mod;     //a           
+            dp[i+1][0] = ((dp[i][1] + dp[i][2])% mod +dp[i][4]) % mod;//a           
             dp[i+1][1] = (dp[i][0] + dp[i][2]) % mod;               //e
             dp[i+1][2] = (dp[i][1] + dp[i][3]) % mod;               //i
             dp[i+1][3] = (dp[i][2]) % mod;                          //o
