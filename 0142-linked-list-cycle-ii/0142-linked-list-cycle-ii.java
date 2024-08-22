4a68e8f72860413dc2d6a620a7748fafcceadf72
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2024-08-22 23:21:48 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (88.57%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -10,27 +10,21 @@
  * }
  */
 public class Solution {
-    
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        boolean flag = false;
         while(fast!=null && fast.next!=null){
             slow = slow.next;
-            fast = fast.next.next;
+            fast=fast.next.next;
             if(slow==fast){
-                flag = true;
-                break;
+                slow = head;
+                while(slow!=fast){
+                    slow = slow.next;
+                    fast=fast.next;
+                }
+                return slow;
             }
         }
-        
-        if(!flag) return null;
-        
-        slow = head;
-        while(slow!=fast){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        return slow;
+        return null;
     }
 }
