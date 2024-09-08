30507bc60f3ee93ab4783827f56fdd3898d683e1
Ankit Gupta <ankitgv.kvtagore@gmail.com>
2024-09-08 13:35:09 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (7.24%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -9,29 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode[] splitListToParts(ListNode root, int k) {
-        ListNode cur = root;
-        int N = 0;
-        while (cur != null) {
-            cur = cur.next;
-            N++;
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int len = 0;
+        ListNode temp = head;
+        while(temp!=null){
+            len++;
+            temp = temp.next;
         }
-
-        int width = N / k, rem = N % k;
-
-        ListNode[] ans = new ListNode[k];
-        cur = root;
-        for (int i = 0; i < k; ++i) {
-            ListNode head = cur;
-            for (int j = 0; j < width + (i < rem ? 1 : 0) - 1; ++j) {
-                if (cur != null) cur = cur.next;
+        int currentSize = len / k;
+        int firstParts = len % k;
+        ListNode ans[] = new ListNode[k];
+        temp = head;
+        for(int i=0;i<k;i++){
+            ListNode newPart = new ListNode(0);
+            ListNode tail = newPart;
+            
+            int size = currentSize;
+            if(firstParts>0){
+                firstParts--;
+                size++;
             }
-            if (cur != null) {
-                ListNode prev = cur;
-                cur = cur.next;
-                prev.next = null;
+            int j =0;
+            while(j<size){
+                tail.next = new ListNode(temp.val);
+                tail = tail.next;
+                j++;
+                temp = temp.next;
             }
-            ans[i] = head;
+            ans[i] = newPart.next;
         }
         return ans;
     }
